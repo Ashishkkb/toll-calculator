c7f1aaf8695b03aa5ca345fc8ae2af0cea343c95
Ashish Kumar Behera <ashish.behera@aniccadata.in>
2023-12-10 22:31:08 +0530

    Width fix

--- src/components/HomePage/Flags.tsx
+++ src/components/HomePage/Flags.tsx
@@ -15,20 +15,23 @@ const Flags = () => {
                     <div className='mx-auto  font-sans text-white text-center mt-4 justify-center'>
                         Our Toll API is the right fit for global businesses with complete toll coverage for 50+ countries across North America, Latin America, Europe <br /> (Including Russian Federation), Australia, New Zealand, and India.<br />
                     </div>
-                    <div className="w-max banner">
-                        <Image
-                            src={Flag1}
-                            className="w-max mt-16"
-                            alt={"flag1"}
-                        />
+                    <div className="overflow-x-hidden w-[1500px]">
+                            <div className="w-max banner">
+                                <Image
+                                    src={Flag1}
+                                    className="w-max mt-16"
+                                    alt={"flag1"}
+                                />
+                            </div>
+                            <div className="w-max banner2nd">
+                                <Image
+                                    src={Flag2}
+                                    className="w-max mt-10"
+                                    alt={"flag1"}
+                                />
+                            </div>
                     </div>
-                    <div className="w-max banner2nd">
-                        <Image
-                            src={Flag2}
-                            className="w-max mt-10"
-                            alt={"flag1"}
-                        />
-                    </div>
+
                 </div>
             </div>
         </>
