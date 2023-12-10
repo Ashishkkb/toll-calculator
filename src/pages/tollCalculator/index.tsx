0757ce755f7b02b8d434635de455878dc8a5ae93
Ashish Kumar Behera <ashish.behera@aniccadata.in>
2023-12-10 06:53:38 +0530

    Toll PLaza In India with Center option

--- src/pages/tollCalculator/index.tsx
+++ src/pages/tollCalculator/index.tsx
@@ -1,5 +1,5 @@
 // pages/toll-calculator/index.tsx
-import React from 'react';
+import React, { useState } from 'react';
 import dynamic from 'next/dynamic';
 import CalculateTollButton from '../../components/CalculateTollButton';
 import TollGuide from '../../components/TollGuide';
@@ -8,12 +8,11 @@ import TollGuide from '../../components/TollGuide';
 const MyMap = dynamic(() => import('../../components/LeafletMap'), { ssr: false });
 
 const TollCalculatorPage: React.FC = () => {
+
   return (
-    <div>
-      <TollGuide />
-      <CalculateTollButton />
-      <div className='h-[600px] w-[600px] border-2 border-black mx-auto'>
-        <MyMap />
+    <div className='w-screen'>
+      <div className='mx-auto min-w-screen justify-center items-center'>
+      <MyMap />
       </div>
     </div>
   );
